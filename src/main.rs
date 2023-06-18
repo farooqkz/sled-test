d7a446c657c0bd85878c4ed812ce0490f7903157
Farooq Karimi Zadeh <fkz@riseup.net>
2023-06-18 15:57:24 +0330

    InIt

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::time::Instant;
+
+fn insert(n: usize, mul: usize, tree: &sled::Tree) {
+    (0..n).for_each(|i| { tree.insert((i*mul).to_le_bytes(), &i.to_le_bytes()).expect("moo"); });
+}
+
+fn read(keys: Vec<i32>, tree: &sled::Tree) {
+    keys.iter().for_each(|key| { tree.get(key.to_le_bytes()).expect("oh no!"); } )
+}
+
+fn main() {
+    let db = sled::open("/tmp/dbdbdb").expect("mooo");
+    let tree = db.open_tree("default").expect("mooo");
+    insert(100, 3, &tree);
+    let vec = (0..10).map(|i| i * 3).collect();
+    let now = Instant::now();
+    read(vec, &tree);
+    println!("100 entries, read 10 of them {}", now.elapsed().as_micros());
+    insert(10000, 10, &tree);
+    let vec = (0..10).map(|i| i * 10).collect();
+    let now = Instant::now();
+    read(vec, &tree);
+    println!("About 10k + 100 entries, read 10 of them {}", now.elapsed().as_micros());
+    insert(1000000, 100, &tree);
+    let vec = (0..10).map(|i| i * 100).collect();
+    let now = Instant::now();
+    read(vec, &tree);
+    println!("About 1M + 10k + 100 entries, read 10 of them {}", now.elapsed().as_micros());
+}
